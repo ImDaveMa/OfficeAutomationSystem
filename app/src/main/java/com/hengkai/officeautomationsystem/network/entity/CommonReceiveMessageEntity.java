b5062f6d4a2c8270eea7433b59040801b6b08f22
harryhasai <harryhasai@qq.com>
2018-05-09 09:13:46 +0800

    通用Entity

---
+++ app/src/main/java/com/hengkai/officeautomationsystem/network/entity/CommonReceiveMessageEntity.java
@@ -0,0 +1,11 @@
+package com.hengkai.officeautomationsystem.network.entity;
+
+/**
+ * Created by Harry on 2018/5/8.
+ * 接收通用消息的页面
+ */
+public class CommonReceiveMessageEntity {
+
+    public int CODE;
+    public String MES;
+}
