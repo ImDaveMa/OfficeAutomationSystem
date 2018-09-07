f12fb95bf8701ca0c62aa46c210db7ad5c3c46aa
harryhasai <harryhasai@qq.com>
2018-09-07 09:20:00 +0800

    修改了Bug

--- app/src/main/java/com/hengkai/officeautomationsystem/final_constant/URLFinal.java
+++ app/src/main/java/com/hengkai/officeautomationsystem/final_constant/URLFinal.java
@@ -18,7 +18,7 @@ public class URLFinal {
      */
     public static final String BASE_URL = "http://192.168.2.157:8080/OA_inside/mobile/" + VERSION + "/";
 //    public static final String BASE_URL = "http://192.168.2.18/OA_inside/mobile/" + VERSION + "/";
-//    public static final String BASE_URL = "http://oa.lyyisun.com/mobile/" + VERSION + "/";
+//    public static final String BASE_URL = "http://oa.lyyisun.com:8042/mobile/" + VERSION + "/";
 
 
     /**
